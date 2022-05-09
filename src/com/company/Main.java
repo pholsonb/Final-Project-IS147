0f6efa33793ecdeced96218f7688a73014a89408
Patrick <pholsonb@comcast.net>
2022-05-08 20:39:20 -0400

    adding functionality

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,42 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        String className;
-        String classTime;
-        String classLength;
-        String startDate;
-        String endDate;
-
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter your class Schedule");
-
-        System.out.println("Class Name: ");
-        className=sc.next();
-
-        System.out.println("Class time: ");
-        classTime=sc.next();
-
-        System.out.println("Class time length");
-        classLength=sc.next();
-
-        System.out.println("Start date: ");
-        startDate=sc.next();
-
-        System.out.println("End date: ");
-        endDate=sc.next();
-
-        System.out.println("Class name: " + className);
-        System.out.println("Class length: " + classLength);
-        System.out.println("Class time: " + classTime);
-        System.out.println("Class start: " + startDate);
-        System.out.println("Class end: " + endDate);
-
-        System.out.println(className);
-
-        System.out.println("Press 1 to continue");
-
-    }
+        input.classes();
+        }
 }
 
--- src/com/company/input.java
+++ src/com/company/input.java
@@ -2,42 +2,49 @@ package com.company;
 
 import java.util.Scanner;
 
-public class input extends Main{
+public class input {
 
     public static void classes(){
-        String className;
-        String classStartTime;
-        String classEndTime;
 
-        String startDate;
-        String endDate;
-        //Scanner sc = new Scanner(System.in);
-        Scanner sc = new Scanner(System.in);
+        int enter;
 
-        System.out.println("Enter your class Schedule");
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("welcome to the Schedule Builder");
+        System.out.println("Type 1 to enter a class schedule");
+        System.out.println("Type 2 to enter a job schedule");
+        System.out.println("Type 3 to enter a chore Schedule");
+        enter = Integer.parseInt(sc1.next());
 
-        System.out.println("Class Name: ");
-        className=sc.next();
+        if(enter == 1) {
 
-        System.out.println("Class start time: ");
-        classStartTime=sc.next();
+            Scanner sc = new Scanner(System.in);
 
-        System.out.println("Class end time: ");
-        classEndTime=sc.next();
+            String className;
+            String classStartTime;
+            String classEndTime;
+            String startDate;
+            String endDate;
 
-        System.out.println("Start date: ");
-        startDate=sc.next();
+            System.out.println("Enter your class Schedule");
 
-        System.out.println("End date: ");
-        endDate=sc.next();
+            System.out.println("Class Name: ");
+            className = sc.next();
 
+            System.out.println("Class start time: ");
+            classStartTime = sc.next();
 
+            System.out.println("Class end time: ");
+            classEndTime = sc.next();
 
-        System.out.println(className);
+            System.out.println("Start date: ");
+            startDate = sc.next();
 
+            System.out.println("End date: ");
+            endDate = sc.next();
 
+            System.out.println(classStartTime + "-" + classEndTime + " " + className);
 
-
+        }
     }
 }
 
